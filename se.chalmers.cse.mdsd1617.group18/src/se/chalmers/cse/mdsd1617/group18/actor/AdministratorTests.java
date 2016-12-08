8d776338f07df458be816eb2e1dc28d7999d52a7
Alexander Håkansson <al3xand3r.h@gmail.com>
2016-12-08 15:59:07 +0100

    Adds initializer for administrator tests

--- se.chalmers.cse.mdsd1617.group18/src/se/chalmers/cse/mdsd1617/group18/actor/AdministratorTests.java
+++ se.chalmers.cse.mdsd1617.group18/src/se/chalmers/cse/mdsd1617/group18/actor/AdministratorTests.java
@@ -11,27 +11,32 @@ import se.chalmers.cse.mdsd1617.group18.roomManager.*;
 import se.chalmers.cse.mdsd1617.group18.roomManager.impl.RoomManagerFactoryImpl;
 
 public class AdministratorTests {
+	
+	private RoomManager roomManager;
 
+	@Before
+	public void initializeAdminTests() {
+		RoomManagerFactoryImpl.init();
+		roomManager = RoomManagerFactoryImpl.getInstance().createRoomManager(null, null);
+	}
+	
+	// UC 2.2.3
 	@Test
 	public void testAdministratorRemoveRoomType() {
-		RoomManagerFactoryImpl.init();
-		RoomManager rM = RoomManagerFactoryImpl.getInstance().createRoomManager(null, null);
-		
-		EList<IRoomType> roomTypes = rM.getRoomTypes();
-		rM.removeRoomType(roomTypes.get(0).getName());
-		EList<IRoomType> newRoomTypes = rM.getRoomTypes();
+		EList<IRoomType> roomTypes = roomManager.getRoomTypes();
+		roomManager.removeRoomType(roomTypes.get(0).getName());
+		EList<IRoomType> newRoomTypes = roomManager.getRoomTypes();
 		assertEquals(false, newRoomTypes.contains(roomTypes.get(0)));
 	}
 	
+	// UC 2.2.4
 	@Test
 	public void testAdministratorAddRoom() {
-		RoomManagerFactoryImpl.init();
-		RoomManager rM = RoomManagerFactoryImpl.getInstance().createRoomManager(null, null);
 		int roomNumber = 0;
 		IRoomType rT = RoomManagerFactoryImpl.getInstance().createRoomType(1000, "Basic room", 1);
-		rM.addRoom(roomNumber, rT);
+		roomManager.addRoom(roomNumber, rT);
 		boolean found = false;
-		EList<IRoom> rooms = rM.getRooms();
+		EList<IRoom> rooms = roomManager.getRooms();
 		for(int i = 0; i < rooms.size(); i++) {
 			IRoom r = rooms.get(i);
 			if (r.getRoomNumber() == roomNumber && r.getRoomType().equals(rT)) {
@@ -41,6 +46,7 @@ public class AdministratorTests {
 		assertEquals(true, found);
 	}
 	
+	// UC 2.1.11
 	@Test
 	public void testAdministratorCheckInRoom() {
 		assertEquals(true, false);
