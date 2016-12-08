28fefbb547a169c5d005a6b53f65bef715f5ffb4
Alexander Håkansson <al3xand3r.h@gmail.com>
2016-12-08 15:19:25 +0100

    Adds test for UC 2.2.3

--- se.chalmers.cse.mdsd1617.group18/src/se/chalmers/cse/mdsd1617/group18/actor/ActorTests.java
+++ se.chalmers.cse.mdsd1617.group18/src/se/chalmers/cse/mdsd1617/group18/actor/ActorTests.java
@@ -1,11 +1,23 @@
 package se.chalmers.cse.mdsd1617.group18.actor;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
+import static org.junit.Assert.assertEquals;
 
-@RunWith(Suite.class)
-@SuiteClasses({})
+import org.eclipse.emf.common.util.EList;
+import org.junit.*;
+
+import se.chalmers.cse.mdsd1617.group18.roomManager.*;
+import se.chalmers.cse.mdsd1617.group18.roomManager.impl.RoomManagerFactoryImpl;
+
 public class ActorTests {
 
+	@Test
+	public void testAdministratorRemoveRoomType() {
+		RoomManagerFactoryImpl.init();
+		RoomManager rM = RoomManagerFactoryImpl.getInstance().createRoomManager(null, null);
+		Administrator admin = new Administrator(rM, rM);
+		EList<RoomType> roomTypes = admin.getRoomManager().getRoomTypes();
+		admin.getRoomManager().removeRoomType(roomTypes.get(0).getName());
+		EList<RoomType> newRoomTypes = admin.getRoomManager().getRoomTypes();
+		assertEquals(false, newRoomTypes.contains(roomTypes.get(0)));
+	}
 }
