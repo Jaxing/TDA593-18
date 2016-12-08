8a71605859a34783435a1762e1ae30fd462a3a6c
jaxing <jaxing@student.chalmers.se>
2016-12-08 18:20:15 +0100

    added test cases to resceptionistTest

--- se.chalmers.cse.mdsd1617.group18/src/se/chalmers/cse/mdsd1617/group18/actor/ReceptionistTest.java
+++ se.chalmers.cse.mdsd1617.group18/src/se/chalmers/cse/mdsd1617/group18/actor/ReceptionistTest.java
@@ -2,6 +2,7 @@ package se.chalmers.cse.mdsd1617.group18.actor;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -10,6 +11,7 @@ import org.junit.runners.Suite.SuiteClasses;
 import se.chalmers.cse.mdsd1617.group18.bookingSystem.Booking;
 import se.chalmers.cse.mdsd1617.group18.bookingSystem.BookingSystem;
 import se.chalmers.cse.mdsd1617.group18.bookingSystem.BookingSystemFactory;
+import se.chalmers.cse.mdsd1617.group18.bookingSystem.IBooking;
 import se.chalmers.cse.mdsd1617.group18.bookingSystem.impl.BookingSystemFactoryImpl;
 import se.chalmers.cse.mdsd1617.group18.bookingSystem.impl.BookingSystemImpl;
 import se.chalmers.cse.mdsd1617.group18.bookingSystem.impl.BookingImpl;;
@@ -17,13 +19,41 @@ import se.chalmers.cse.mdsd1617.group18.bookingSystem.impl.BookingImpl;;
 
 public class ReceptionistTest {
 	
+	private BookingSystemFactory bookingSystemFactory;
+	private BookingSystem bookingSystem;
+	
+	@Before
+	public void initReceptionist () {
+		bookingSystemFactory = BookingSystemFactoryImpl.init();
+		bookingSystem = bookingSystemFactory.createBookingSystem();
+	}
+	
 	@Test
-	public void createBooking () {
-		BookingSystemFactory bookingSystemFactory = BookingSystemFactoryImpl.init();
-		BookingSystem bookingSystem = bookingSystemFactory.createBookingSystem();
-		
+	public void createBooking () {		
 		int id = bookingSystem.initiateBooking("FisrtName", "20161208", "20161223", "LastName");
 		
+		//TODO: Need a factory to create a booking with given paramenters.
 		assertTrue((Booking)bookingSystem.listBooking().get(0) == bookingSystemFactory.createBooking() );
+	}
+	
+	//*** List Bookings
+	@Test
+	public void listBookingsIsNotEmpty () {
+		int id = bookingSystem.initiateBooking("FisrtName", "20161208", "20161223", "LastName");
+		bookingSystem.checkInRoom("RoomType", id);
+		
+		assertTrue(!bookingSystem.listBooking().isEmpty());
+	}
+	
+	
+	//*** List Occupied Room For Specified Day
+	@Test
+	public void listOccupiedRoomsForSpecifiedDay () {
+		
+	}
+	
+	@Test
+	public void unblockRoom () {
+		
 	}
 }
