60dfd1d83c903bd5db329b80afae2047e619bb32
Alexander Håkansson <al3xand3r.h@gmail.com>
2016-12-08 14:42:25 +0100

    Adds the required JAR files

---
+++ se.chalmers.cse.mdsd1617.group18/src/se/chalmers/cse/mdsd1617/group18/actor/ActorTests.java
@@ -0,0 +1,11 @@
+package se.chalmers.cse.mdsd1617.group18.actor;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({})
+public class ActorTests {
+
+}
